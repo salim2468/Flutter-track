a298e4420ceb13a95559cd0db0263d2fb38c82fb
salim2468 <salimmaharjan2468@gmail.com>
2023-10-03 21:14:42 +0545

    bottom tab added along with blank page profile

---
+++ lib/pages/bottom_navigation.dart/bottom_nav.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_track/pages/dashboard_page.dart';
+
+import '../add_page.dart';
+import '../profile_page.dart';
+
+class BottomNav extends StatefulWidget {
+  const BottomNav({super.key});
+
+  @override
+  State<BottomNav> createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  int _selectedIndex = 0;
+  static final List<Widget> _pages = <Widget>[
+    const DashboardPage(),
+    AddPage(),
+    const ProfilePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey.shade700,
+        showUnselectedLabels: false,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'Add',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
